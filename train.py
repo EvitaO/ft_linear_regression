602ba8d3da6eb6685f55cec532db4aa563057e6f
Evita Overtoom <eovertoo@f1r6s11.codam.nl>
2022-04-04 21:01:11 +0200

    save

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from re import X
+import matplotlib.pyplot as plt
 
 
 def readfile():
@@ -21,14 +22,20 @@ def calculateNorm(value, dataset):
 def	normalize(x, y):
 	normalized = []
 	for i,j in zip(x,y):
-		normalized.append([calculateNorm(int(i), x), calculateNorm(int(j), y)])
+		normalized.append([calculateNorm(float(i), x), calculateNorm(float(j), y)])
 	return normalized
 
 def	main():
 	x,y = readfile()
 	normalized = normalize(x,y)
+	price = []
+	km = []
 	for i in normalized:
+		km.append(i[0])
+		price.append(i[1])
 		print (i)
+	plt.scatter(km, price)
+	plt.show()
 	# theta0, theta1 = calculateRegressionLine(x, y)
 	# print("theta0 = " + str(theta0) + "	theta1 = " + str(theta1))
 
