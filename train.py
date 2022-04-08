e1344a7de8a149d74bfc6356a67566e7fd091c36
Evita Overtoom <eovertoo@f1r6s11.codam.nl>
2022-04-08 22:45:27 +0200

    save

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ from re import X
 import matplotlib.pyplot as plt
 import numpy as np
 
+theta0 = 0
+theta1 = 0
+learningRate = 0.5
+
 def readfile():
 	file = open("data.csv")
 	next(file)
@@ -25,6 +29,22 @@ def	normalize(x, y):
 		normalized.append([calculateNorm(float(i), x), calculateNorm(float(j), y)])
 	return normalized
 
+def estimatePrice(mileage):
+	return theta0 + (theta1 * mileage)
+
+def	calculateTheta(km, price):
+	tmp = 0
+	tmp1 = 0
+	global theta0
+	global theta1
+
+	for i,j in zip(km, price):
+		tmp = (estimatePrice(i) - j)
+		tmp1 = ((estimatePrice(i) - j) * i)
+		theta0 = theta0 - learningRate * (tmp/len(km))
+		theta1 = theta1 - learningRate * (tmp1/len(price))
+	return theta0, theta1
+
 def	main():
 	x,y = readfile()
 	normalized = normalize(x,y)
@@ -34,9 +54,12 @@ def	main():
 		km.append(i[0])
 		price.append(i[1])
 		print (i)
-	plt.scatter(km, price)
-	plt.show()
+	# plt.scatter(km, price)
+	# plt.show()
 
+	theta0, theta1 = calculateTheta(km, price)
+	print(theta0)
+	print(theta1)
 	# formula found using excel
 	# y = -0.0214 * x + 8500
 	# x = np.array(range(300000))
